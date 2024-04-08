a33915f2c888b894c8710f18421eae41f4346ae3
Aaron Studer <aaron.studer@ergon.ch>
2024-04-08 14:47:54 +0200

    add TODO for fetching forecast if someone else wants to do it

--- src/page/overview/MswSpotlist.js
+++ src/page/overview/MswSpotlist.js
@@ -77,7 +77,66 @@ function Spot(location, flowAndTempData) {
 
   const fetchForecastData = async(locationId) => {
     if(!forecastDataMap.get(locationId)) {
-      let fetch_url = `https://europe-west6-windy-anchor-328011.cloudfunctions.net/msw_forecast?locationId=${locationId}`;
+      // explanation:
+      // - This fetch-url is used to fetch the forecast data.
+      // - unfortunately we can't fetch the forecast data directly from "https://www.hydrodaten.admin.ch/plots/q_forecast/" + locationId + "_q_forecast_de.json", because they protected their API using CORS
+      // - But a backend (or a cloud function can fetch the data from there)
+      // - therefore we call a cloud function that fetches and returns the data for us.
+      // let fetch_url = `https://europe-west6-windy-anchor-328011.cloudfunctions.net/msw_forecast?locationId=${locationId}`;
+      // TODO: use different fetch_url with your own backend.
+      //  You can use this one for some local testing, but please don't use it productively and make one on your own,
+      //  as it's now running in a cloud function on GCP which costs me per use...
+      //  My Cloud-Function looks like this:
+            // package gcfv2;
+            //
+            // import com.google.cloud.functions.HttpFunction;
+            // import com.google.cloud.functions.HttpRequest;
+            // import com.google.cloud.functions.HttpResponse;
+            // import java.io.BufferedReader;
+            // import java.io.InputStreamReader;
+            // import java.net.HttpURLConnection;
+            // import java.net.URL;
+            // import java.util.logging.Logger;
+            //
+            // public class HelloHttpFunction implements HttpFunction {
+            //   private static final Logger logger = Logger.getLogger(HelloHttpFunction.class.getName());
+            //
+            //   @Override
+            //   public void service(final HttpRequest request, final HttpResponse response) throws Exception {
+            //      String locationId = request.getFirstQueryParameter("locationId").orElse("");
+            //         logger.info(locationId);
+            //         String urlStr = "https://www.hydrodaten.admin.ch/plots/q_forecast/" + locationId + "_q_forecast_de.json";
+            //
+            //         URL url = new URL(urlStr);
+            //         HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            //         con.setRequestMethod("GET");
+            // 				con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0");
+            //
+            //
+            //         int responseCode = con.getResponseCode();
+            //         logger.info("GET Response Code :: " + responseCode);
+            //
+            // 				response.appendHeader("Access-Control-Allow-Origin", "*");
+            //         if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            //             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            //             String inputLine;
+            //             StringBuffer data = new StringBuffer();
+            //
+            //             while ((inputLine = in.readLine()) != null) {
+            //                 data.append(inputLine);
+            //             }
+            //             in.close();
+            //
+            //             logger.info("inside end");
+            //             response.getWriter().write(data.toString());
+            //         } else {
+            //             logger.info("GET request didn't work");
+            //             response.setStatusCode(HttpURLConnection.HTTP_INTERNAL_ERROR);
+            //             response.getWriter().write("Error: GET request didn't work");
+            //         }
+            //   }
+            // }
+
       try {
         const response = await fetch(fetch_url);
         let responseData = await response.json();
